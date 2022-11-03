bb49bdc0b91c768e464b4edea19789f7c595dd52
unknown <shyusupova4@gmail.com>
2022-11-03 17:34:27 +0500

    Fix linter errors

--- morsecode_decoder.rb
+++ morsecode_decoder.rb
@@ -16,7 +16,7 @@ end
 puts decode_char('.-')
 
 # Create a method to decode an entire word in Morse code, takes a string parameter,
-# and return the string representation. Every character in a word will be separated by a single space 
+# and return the string representation. Every character in a word will be separated by a single space
 # (e.g. decode_word("-- -.--") returns "MY").
 
 def decode_word(morse_word)
@@ -33,15 +33,7 @@ puts decode_word('-- -.--')
 # and return the string representation. Every word will be separated by 3 spaces (e.g. decode("-- -.--   -. .- -- .")
 
 def decode_message(morse_message)
-  morse_message
-    .split('  ')
-    .reject do |char|
-      char == ''
-    end
-    .map do |word|
-      word.split.map { |char| DICTIONARY[char] }.join
-    end
-    .join(' ')
+  morse_message.split('   ').map { |word| decode_word(word) }.join(' ')
 end
 
-puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
